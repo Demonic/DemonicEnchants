885db62016a23f49cefe63fe2c441ee6c5884fad
Demonic <admin@Demonly.net>
2021-03-03 23:51:16 -0500

    init

---
+++ src/main/java/net/Demonly/DemonicEnchants.java
@@ -0,0 +1,18 @@
+package net.Demonly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+//
+public class DemonicEnchants extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // do something
+    }
+
+    public void onDisable()
+    {
+        // do something
+    }
+}
